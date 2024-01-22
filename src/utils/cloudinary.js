4a180a4bee4f110ead9965d976b1d9f9b760875f
RupakGhosh4865 <rupak4865@gmail.com>
2024-01-23 00:58:47 +0530

    file handling

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,54 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from "fs" 
+
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadoncloudinary = async (localfilePath) => {
+
+try{
+    if(!localfilePath)
+        return null
+
+const response =await cloudinay.uploader.upload(localfilePath,{
+resource_type: "auto"
+}) 
+
+console.log("file is upload on cloudinary",response.url);
+return response;
+}
+
+    catch (error) {
+        fs.unlinkSync(localfilePath) //remove the locally saved temporary files
+     
+    return null;
+    }
+}
+
+
+
+
+
+
+export {uploadoncloudinary}
+
+
+
+
+
+
+
+
+
+
+
+
+
+cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
